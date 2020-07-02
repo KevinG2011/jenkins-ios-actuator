cc1718af1c6900faab76fabfb48275847f1fce36
lijia <lijia@huajiao.tv>
2020-07-02 13:44:23 +0800

    修复正则表达式匹配Version的bug.

--- src/main/java/com/pepper/symbol/IOSSymbolConstants.java
+++ src/main/java/com/pepper/symbol/IOSSymbolConstants.java
@@ -2,7 +2,7 @@ package com.pepper.symbol;
 
 public class IOSSymbolConstants {
     public static final String SIGNATURE = "com.huajiao.seeding";
-    public static final String REGEX_VERSION = "Version:\\s+\\d{10}";
+    public static final String REGEX_VERSION = "Version:.+\\d{10}";
     public static final String DSYM_FILENAME = "living.app.dSYM";
 
     private IOSSymbolConstants() {
